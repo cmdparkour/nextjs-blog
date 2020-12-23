537077efe292b729cc33b2947ffd94376720bed5
gu_jitao <2421500392@qq.com>
2020-12-23 20:08:10 +0800

    save my code

---
+++ components/date.js
@@ -0,0 +1,10 @@
+import { parseISO, frmat, format } from 'date-fns'
+
+export default function Date({ dateString }) {
+    const date = parseISO(dateString)
+    return (
+        <time dateTime={dateString}>
+            {format(date, 'LLLL d, yyyy')}
+        </time>
+    )
+}
